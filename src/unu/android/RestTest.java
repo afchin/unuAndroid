7ab9c7988530c640a00a621d59f3018d74151e51
Annie Chin <spazznie@gmail.com>
2012-04-14 13:49:20 -0700

    Initial REST client

---
+++ src/unu/android/RestTest.java
@@ -0,0 +1,16 @@
+package unu.android;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+ 
+import unu.android.RestClient;
+
+import static org.junit.Assert.assertTrue;
+
+public class RestTest {
+	public void testClientGet(){
+		
+	}
+}
