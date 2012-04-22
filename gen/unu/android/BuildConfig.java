537e2d7468009fe4f3087c95fd48b7c9d8bfd0a1
Annie Chin <spazznie@gmail.com>
2012-04-22 16:59:50 -0700

    initial patches implementation, works with latest adk

---
+++ gen/unu/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package unu.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/unu/android/PatchesFragment.java
+++ src/unu/android/PatchesFragment.java
@@ -1,8 +1,8 @@
 package unu.android;
 
 import android.app.Activity;
-import android.app.ListFragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,25 +16,25 @@ import android.widget.LinearLayout;
 
 public class PatchesFragment extends ListFragment {
 
-//  @Override
-//  public void onCreate(Bundle savedInstanceState) {
-//    super.onCreate(savedInstanceState);
-//
-//  }
-//
-//  @Override
-//  public void onActivityCreated(Bundle savedInstanceState) {
-//    super.onActivityCreated(savedInstanceState);
-//    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-//        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-//        "Linux", "OS/2" };
-//    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-//        android.R.layout.simple_list_item_1, values);
-//    setListAdapter(adapter);
-//  }
-//
-//  @Override
-//  public void onListItemClick(ListView l, View v, int position, long id) {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+        "Linux", "OS/2" };
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+        android.R.layout.simple_list_item_1, values);
+    setListAdapter(adapter);
+  }
+
+  @Override
+  public void onListItemClick(ListView l, View v, int position, long id) {
 //    String item = (String) getListAdapter().getItem(position);
 //    DetailFragment fragment = (DetailFragment) getFragmentManager()
 //        .findFragmentById(R.id.detailFragment);
@@ -48,5 +48,5 @@ public class PatchesFragment extends ListFragment {
 //
 //    }
 
-//  }
+  }
 }
